b4ff961558bb8fadcae1099f824484875a466595
unknown <mitsevskyi.corporate@gmail.com>
2023-04-27 01:46:46 +0300

    feat: create bot and converter from ogg to mp3

---
+++ src/openai.js
@@ -0,0 +1,15 @@
+class OpenAI {
+    constructor () {
+
+    }
+
+    chat () {
+
+    }
+
+    transcription () {
+        
+    }
+}
+
+export const openai = new OpenAI();
---
+++ src/utils.js
@@ -0,0 +1,10 @@
+import { unlink } from 'fs/promises';
+
+export async function removeFiles(path) {
+    try {
+        await unlink(path);
+    }
+    catch (e) {
+        console.log('Error while removing file', e.message);
+    }
+}
